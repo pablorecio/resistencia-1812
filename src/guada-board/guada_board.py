a114fae7e54c6c93ef06cce8376b78d57e2b9e9c
riku <riku@8c032950-cc2e-4ed2-a043-66bdc8b6e3a5>
2009-11-19 18:12:35 +0000

    Added name of the players on the screen, but fails something in team B. Backup commit
    git-svn-id: https://forja.rediris.es/svn/cusl4-res-cadiz/trunk@49 8c032950-cc2e-4ed2-a043-66bdc8b6e3a5

--- src/guada-board/guada_board.py
+++ src/guada-board/guada_board.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+import pygame.font
 import board
 
 import sys
@@ -9,12 +10,49 @@ sys.path.append("./libguadalete")
 import libguadalete
 import game
 
+#-------- Sizes declarations -----------------
 screen_size = (760,560)
 board_ub = (10,10)
+frame_A = (10,500)
+piece_frame_A = (17,506)
+text_frame_A = (57,506)
+frame_B = (251,500)
+piece_frame_B = (258,506)
+text_frame_B = (298,206)
+#----------------------------------------------
+# ------ Colors -------------------------------
+font_name_color = (0,0,0)
+#----------------------------------------------
+#------- Files and variables ------------------
+path_favicon = 'images/favicon.png'
+path_piece_A = 'images/piece-orange.png'
+path_piece_B = 'images/piece-violete.png'
+path_piece_default = 'images/piece-default.png'
+path_background = 'images/fondo.png'
+path_frame = 'images/marco.png'
+path_font_names = 'fonts/zektonbi.ttf'
+#----------------------------------------------
+
+def generate_background_surface(surfaces):
+    surface = pygame.Surface(screen_size)
+    
+    surface.blit(surfaces['background'],(0,0))
+    surface.blit(surfaces['frame'], frame_A)
+    surface.blit(surfaces['frame'], frame_B)
+    surface.blit(surfaces['piece_A'], piece_frame_A)
+    surface.blit(surfaces['piece_B'], piece_frame_B)
+    surface.blit(surfaces['name_team_A'], text_frame_A)
+    surface.blit(surfaces['name_team_B'], text_frame_B)
+
+    return surface
+
+def extract_name(team):
+    i = team[1].find("/equipo")
+    j = team[1].find(".clp")
+
+    return (team[1])[i+7:j]
 
 def run(teamA, teamB):
-    print teamA
-    print teamB
     lib = libguadalete.LibGuadalete(teamA,teamB)
     out_file = lib.run_game()
 
@@ -22,16 +60,30 @@ def run(teamA, teamB):
     
     pygame.init()
 
+    surfaces = {}
+    name_team_A = extract_name(teamA)
+    name_team_B = extract_name(teamB)
+
     screen = pygame.display.set_mode(screen_size)
     pygame.display.set_caption('Resistencia en Cadiz: 1812 (Tablero)')
-    favicon = pygame.image.load('images/favicon.png').convert()
+    favicon = pygame.image.load(path_favicon).convert()
     pygame.display.set_icon(favicon)
 
-    res_game = game.Game(entire_game, './images/piece-orange.png',
-                         './images/piece-violete.png',
-                         './images/piece-default.png')
+    res_game = game.Game(entire_game, path_piece_A,
+                         path_piece_B, path_piece_default)
 
-    background = pygame.image.load('images/fondo.png').convert()
+    surfaces['background'] = pygame.image.load(path_background).convert()
+    surfaces['frame'] = pygame.image.load(path_frame).convert()
+    piece_surf_frame_A = pygame.image.load(path_piece_A).convert()
+    surfaces['piece_A'] = pygame.transform.scale(piece_surf_frame_A,(35,35))
+    piece_surf_frame_B = pygame.image.load(path_piece_B).convert()
+    surfaces['piece_B'] = pygame.transform.scale(piece_surf_frame_B,(35,35))
+
+    name_font = pygame.font.Font(path_font_names, 22)
+    surfaces['name_team_A'] = name_font.render(name_team_A, 1, font_name_color)
+    surfaces['name_team_B'] = name_font.render(name_team_B, 1, font_name_color)
+
+    background = generate_background_surface(surfaces)
     img_board = res_game.draw_board().convert()
 
     screen.blit(background,(0,0))
